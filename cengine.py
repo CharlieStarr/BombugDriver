32cab3ac682318b7b4ef249fc577ee071fac8a93
CharlieStarr <contactocarlose@gmail.com>
2024-11-22 18:58:06 -0500

    Update cengine.py

--- cengine.py
+++ cengine.py
@@ -2,37 +2,18 @@
 Custom graphics library for the Bombug.
 Made (completly) by Carlos ★.
 
-Using pygame library for graphics management.
-
 Used for managing projection and graphics 
 math and other calculations.
 """
 
-import math as m    # Math
-import pygame as pg # Allows graphic management
+import math as m
 
-# Function to calculate color of object according to depth
-def colorFromDepth(baseColor, zCoord):
-    newColor = []
-    for color in baseColor:
-        if color != 0 and zCoord !=0:
-            value = (color*255)/(m.sqrt(abs(zCoord))*105)
-            # TO DO: Fix whatever this mess is
-            if value > 255:
-                newColor.append(255)
-            else:
-                newColor.append(value)
-        else:
-            newColor.append(0)
-    return newColor
-
-# Function to calculate position according to distances from ultrasound sensors
-def zCoordinate(firstd1, firstd2, a):
-    d1 = firstd1
-    d2 = firstd2
-    x = ((d1**2) - (d2**2))/(4*a)
-    z = (m.sqrt(abs((d1)**2-((a+x)**2))))
-    return [x, 0, z, 0, 0, 0]
+"""
+CLASSES:
+Each class represents a specific object 
+either pre-projection (3d) or 
+after-projection (2d)
+"""
 
 # Node class that represents one point of the displayed object
 class Node:
@@ -43,11 +24,13 @@ class Node:
         self.x = 0
         self.y = 0
         self.z = 0
-        self.alpha = 0
+        self.alpha = 0         # More coordinates, but these are angle coordinates
         self.beta = 0
         self.gamma = 0
         self.pos = [0, 0, 0]
-    def move(self, center):
+
+    # Function to move a point to specified coordinates
+    def move(self, center): 
         self.alpha = center[3]
         self.beta = center[4]
         self.gamma = center[5]
@@ -56,8 +39,8 @@ class Node:
         self.z = center[2] + self.zshift
         self.pos = [self.x, self.y, self.z]
 
-# Displayable class that represents a to-be-displayed object
-class Displayable():
+# TransformedNode class that represents a node passed through a Camer Transform
+class TransformedNode():
     def __init__(self, point, camera, display):
         X = point.x - camera.x # More coordinates
         Y = point.y - camera.y
@@ -78,20 +61,69 @@ class Displayable():
         self.size = display.z
         # print("Positions: ", self.x, self.y, self.z) Debugging only
 
-# Function to calculate the perspective projection
-def perspective(self):
-    if self.z != 0:
-        x = (self.size*self.x/self.z)
-        y = (self.size*self.y/self.z)
-        z = self.z
-    else:
-        x, y, z = 0, 0, self.z
-    return (x, y, z)
-
-# Display class that collets projected point for projection as a pygame polygon
-class Display(pg.sprite.Sprite):
+# Face class that collets projected points for displaying them through pygame
+class Face():
     def __init__(self, color, nodeList):
         self.color = color
         self.nodes = []
         for node in nodeList:
             self.nodes.append(Node(node))
+
+"""
+FUNCTIONS:
+These functions are just the bulk of the
+math that radar.py needs to project images
+"""
+
+# Function to calculate color of object according to depth
+def colorFromDepth(baseColor, zCoord):
+    newColor = []
+    for color in baseColor:
+        if color != 0 and zCoord !=0:
+            value = (color*255)/(m.sqrt(abs(zCoord))*105) 
+            # TODO: Fix whatever this mess is
+            if value > 255:
+                newColor.append(255)
+            else:
+                newColor.append(value)
+        else:
+            newColor.append(0)
+    return newColor
+
+# Function to calculate position according to distances from ultrasound sensors
+def zCoordinate(firstd1, firstd2, a):
+    d1 = firstd1
+    d2 = firstd2
+    x = ((d1**2) - (d2**2))/(4*a)
+    z = (m.sqrt(abs((d1)**2-((a+x)**2))))
+    return [x, 0, z, 0, 0, 0]
+    # TODO: Add a third sensor to detect y values and extra objects
+
+# Function to calculate the perspective projection
+def perspective(self):
+    if self.z != 0:
+        x = (self.size*self.x/self.z)
+        y = (self.size*self.y/self.z)
+    else:
+        x, y = 0, 0
+    return (x, y)
+
+# Function to project objects and prepare them for displaying
+def project(self, center, camera, screen):
+    points = [] # List of projected points
+
+    # Moves object to user issued position and runs projection function
+    for node in self.nodes: 
+        node.move(center)
+        nodeDisplay = TransformedNode(node, camera, screen)
+
+        # This line only adds points that are in front of the camera 
+        if nodeDisplay.z > 0: 
+            points.append(perspective(nodeDisplay))  
+
+    # Checks if enough points are in front of the camera
+    if len(points) < 3:
+        return None
+    else:
+        color = colorFromDepth(self.color, center[2])
+        return (points, color)     
